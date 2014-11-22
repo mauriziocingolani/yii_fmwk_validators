664f2747517143e56fb506416db13b234372d509
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2014-11-22 11:34:38 +0100

    Modifica validazione email (restituisce motivo fallimento)

--- EmailValidator.php
+++ EmailValidator.php
@@ -3,9 +3,18 @@
 /**
  * Estende CEmailValidator e aggiunge il controllo dell'esistenza del dominio.
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
- * @version 1.0.2
+ * @version 1.0.3
  */
 class EmailValidator extends CEmailValidator {
+
+    /** Valore restituito dal metodo {@link validateValueWithResponse} se l'indirizzo è valido */
+    const ERROR_NONE = 'ok';
+
+    /** Valore restituito dal metodo {@link validateValueWithResponse} se l'indirizzo è sintatticamente errato */
+    const ERROR_SYNTAX = 'syntax';
+
+    /** Valore restituito dal metodo {@link validateValueWithResponse} se il dominio dell'indirizzo non esiste */
+    const ERROR_DOMAIN = 'domain';
 
     /**
      * Se la validazione della superclass ha avuto successo (indirizzo formalmente valido)
@@ -21,6 +30,27 @@ class EmailValidator extends CEmailValidator {
             return checkdnsrr($host);
         endif;
         return false;
+    }
+
+    /**
+     * Implementa le stesse funzionalità del metodo {@link validateValue}, ma restituisce
+     * il motivo per cui la validazione è fallita:
+     * <ul>
+     * <li>{@link EmailValidator::ERROR_SYNTAX}: sintassi errata</li>
+     * <li>{@link EmailValidator::ERROR_DOMAIN}: dominio inesistente</li>
+     * <li>{@link EmailValidator::ERROR_NONE}: indirizzo valido</li>
+     * </ul>
+     * @param type $value Valore da validare
+     * @return string Errore di validazione (vedi costanti della classe)
+     */
+    public function validateValueWithResponse($value) {
+        if ($value == null || strlen(trim($value)) == 0)
+            return self::ERROR_NONE;
+        if (parent::validateValue($value)) :
+            list($address, $host) = explode('@', $value);
+            return checkdnsrr($host) ? self::ERROR_NONE : self::ERROR_DOMAIN;
+        endif;
+        return self::ERROR_SYNTAX;
     }
 
 }
